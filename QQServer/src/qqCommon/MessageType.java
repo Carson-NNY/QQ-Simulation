75ba996ee8dd24d61d9708f7892aa9bfd12e6aa5
Carson-NNY <109368070+Carson-NNY@users.noreply.github.com>
2022-09-11 19:32:00 -0400

    Update MessageType.java

--- QQServer/src/qqCommon/MessageType.java
+++ QQServer/src/qqCommon/MessageType.java
@@ -7,9 +7,9 @@ package qqCommon;
 public interface MessageType {
     String MESSAGE_LOGIN_SUCCESS = "1";
     String MESSAGE_LOGIN_FAIL = "2";
-    String MESSAGE_COMM_MES = "3";  // 普通信息包
-    String MESSAGE_GET_ONLINE_FRIEND = "4"; // 请求返回用户列表
-    String MESSAGE_RET_ONLINE_FRIEND = "5";// 返回在线用户列表
+    String MESSAGE_COMM_MES = "3";  // general messages
+    String MESSAGE_GET_ONLINE_FRIEND = "4"; // Request to return user list
+    String MESSAGE_RET_ONLINE_FRIEND = "5";// Return to the list of online users
     String MESSAGE_CLIENT_EXIT = "6";
     String MESSAGE_CLIENT_GroupCHAT = "7";
     String MESSAGE_CLIENT_FileSending = "8";
