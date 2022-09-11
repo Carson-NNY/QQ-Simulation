95d9d05892a9dec4f5ddeb710f6f118640274f15
Carson-NNY <109368070+Carson-NNY@users.noreply.github.com>
2022-09-11 19:32:51 -0400

    Update User.java

--- QQServer/src/qqCommon/User.java
+++ QQServer/src/qqCommon/User.java
@@ -2,17 +2,14 @@ package qqCommon;
 
 import java.io.Serializable;
 
-/**
- * @author Carson
- * @Version
- */
+
 public class User implements Serializable {
     private String userId;
     private String passwd;
 
-    private static final long serialVersionUID = 1L;    // 增强稳定性
+    private static final long serialVersionUID = 1L;    
 
-    public User(){  // 提供无参构造器然后在别的类里用Setter的方法来规制属性的思想
+    public User(){  
     }
     public User(String userId, String passwd) {
         this.userId = userId;
