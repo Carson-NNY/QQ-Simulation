700cd882afac20d845aecf93f4cc39e8682d26a4
Carson-NNY <109368070+Carson-NNY@users.noreply.github.com>
2022-09-11 18:44:34 -0400

    Update FileClientService.java

--- QQClient/src/qqclient/service/FileClientService.java
+++ QQClient/src/qqclient/service/FileClientService.java
@@ -6,10 +6,6 @@ import qqCommon.MessageType;
 import java.io.*;
 import java.util.Scanner;
 
-/**
- * @author Carson
- * @Version
- */
 public class FileClientService {
 
     public void fileTransfer(String src, String dest, String senderId,String getterId){
@@ -22,17 +18,16 @@ public class FileClientService {
         message.setMesType(MessageType.MESSAGE_CLIENT_FileSending);
 
         FileInputStream fileInputStream =null;
-        // 跟之前IO流用法不一样，这里没用老韩给的工具包，而是自己进行读取的一个方式，记下！
-        byte[] bytes = new byte[(int)new File(src).length()]; // 这样能确定byte正好是文件的大小
+        byte[] bytes = new byte[(int)new File(src).length()]; // This makes sure that byte is exactly the size of the file.
         try {
             fileInputStream = new FileInputStream(src);
-            fileInputStream.read(bytes);    // 这里把源文件的数据写入了bytes
+            fileInputStream.read(bytes);    // Here, the data from the source file is written to the bytes
 
             message.setBytes(bytes);
         } catch (Exception e) {
             e.printStackTrace();
         }finally {
-            // 如果创建成功，要记得关流
+            // If the creation is successful, remember to close the stream
             try {
                 fileInputStream.close();
             } catch (IOException e) {
@@ -40,9 +35,9 @@ public class FileClientService {
             }
         }
 
-        System.out.println(senderId+" 给 "+getterId+" 发送文件 "+src +" 到对面的电脑目录 "+dest);
+        System.out.println(senderId+" send a file to "+getterId +" to the directory: "+dest);
 
-        // 发出还是通过Message对象发送出去
+        //  send out via Message object
         try {
             ObjectOutputStream oos = new ObjectOutputStream(ManageThread.getClientServiceThread(senderId).getSocket().getOutputStream());
             oos.writeObject(message);
