e8e37bd4f2ec88ba4eafd5be39c1a8fc498d3b3a
Carson-NNY <109368070+Carson-NNY@users.noreply.github.com>
2022-09-11 19:03:18 -0400

    Update QQView.java

--- QQClient/src/qqclient/view/QQView.java
+++ QQClient/src/qqclient/view/QQView.java
@@ -7,88 +7,82 @@ import qqclient.utils.Utility;
 
 import java.util.Scanner;
 
-/**
- * @author Carson
- * 大量知识点，反复复习！！
- *
- */
+
 public class QQView {
     private boolean loop = true;
-    private String key = "";    // 接收用户键盘的输入
-    private  UserClientService userClientService = new UserClientService();  // 用于登陆服务器和注册
+    private String key = "";    // Receive input from the user
+    private  UserClientService userClientService = new UserClientService();  // Used to log in to the server 
     private MessageClientService messageClientService = new MessageClientService();
     private    FileClientService fileClientService  = new FileClientService();
 
-    // OOP思想： 老韩的思路！绝了，多复习：
-    // 1: 利用UserClientService类创建了一个user的对象，并且在类的方法中利用输出流发送出这个对象
-    // 2： 再得到输入流来判断是否user登陆成功，如果成功的话就创建一个线程（保持通信），并且把这个线程
-    // 放入一个线程的集合类，以便后期实现多线程
+    // OOP concept:： ：
+    // 1:  create a user object using the UserClientService class, and send it out using the output stream in the class's methods
+    // 2： get the input stream to determine whether the user login is successful, if it is successful, create a thread (keep communication), and put the thread.
+// into a collection class of threads for later implementation of multithreading
 
     public static void main(String[] args) {
-        // 利用main方法来测试
         new QQView().mainMenu();
-        System.out.println("退出系统成功");
+        System.out.println("Exit the system successfully");
     }
 
     private void mainMenu() {
 
         while (loop){
-            System.out.println("================欢迎登陆网络通信系统================");
-            System.out.println("\t\t 1 登陆系统");
-            System.out.println("\t\t 9 退出系统");
-            System.out.print("请输入: ");
+            System.out.println("================Welcome to the network communication system.================");
+            System.out.println("\t\t 1 Log in system");
+            System.out.println("\t\t 9 Exit the system");
+            System.out.print("Please enter : ");
 
             key = Utility.readString(1);
             switch (key){
                 case "1":
-                    System.out.println("请输入用户号： ");
+                    System.out.println("Please enter the user ID： ");
                     String userId = Utility.readString(20);
-                    System.out.println(" 请输入密码： ");
+                    System.out.println(" Enter Your password： ");
                     String userPwd = Utility.readString(20);
 
-                    // 先梳理逻辑～
+
                     if(userClientService.checkUser(userId,userPwd))
-                    // 在参数里面创建了线程，开启start（）进行持续的通信，并且把单个线程放进了集合里，而且把
-                        //里面的公有字段（User，Socket）设置好了，妙！
+                    // Create a thread in the parameter,  start () for continuous communication, put a single thread into the collection, 
+                        // and set the public field (User,Socket) inside
                     {
-                        System.out.println("================欢迎用户： "+userId+" 登陆成功================");
-                        // 进入二级菜单
+                        System.out.println("================Welcome user： "+userId+" Log in successfully================");
+                        // Go to the secondary menu
                         while(loop){
-                            System.out.println("\n================网络通信系统二级菜单================");
-                            System.out.println("\t\t 1 显示在线用户列表");
-                            System.out.println("\t\t 2 群发消息");
-                            System.out.println("\t\t 3 私聊消息");
-                            System.out.println("\t\t 4 发送文件");
-                            System.out.println("\t\t 9 退出系统");
-                            System.out.println("请输入你的选择： ");
+                            System.out.println("\n================Second-level menu of network communication system================");
+                            System.out.println("\t\t 1 Display a list of online users");
+                            System.out.println("\t\t 2 Send messages in groups");
+                            System.out.println("\t\t 3 Chat in private.");
+                            System.out.println("\t\t 4 Send a file");
+                            System.out.println("\t\t 9 exit");
+                            System.out.println("Please enter your choice： ");
                             key = Utility.readString(1);
                             switch (key){
                                 case "1":
-                                    // 注意： 必须要用userClientService，因为设置的全局变量，所有的属性
-                                    // 都在里面！
+                                    // Note: userClientService must be used, because of global variable, all the properties are in it!
                                     userClientService.onlineFriendList();
 
                                     break;
                                 case "2":
-                                    System.out.print("请输入想说的话： ");
+                                    System.out.print("Please enter what you want to say： ");
                                     String content = Utility.readString(100);
                                     messageClientService.groupChat(content,userId);
                                     break;
                                 case "3":
-                                    System.out.print("请输入想聊天的对象： ");
+                                    System.out.print("Please enter the person you want to chat with： ");
                                     String getter  = Utility.readString(50);
-                                    System.out.print("请输入想说的话： ");
+                                    System.out.print("Please enter what you want to say： ");
                                     String sentence = Utility.readString(100);
                                     messageClientService.privateChat(sentence,userId,getter);
 
                                     //userClientService.privateChat();
                                     break;
                                 case "4":
-                                    System.out.println("请输入你想把文件发送给的用户");
+                                    System.out.println("Please enter the user to whom you want to send the file");
                                      getter = Utility.readString(60);
-                                    System.out.println("输入这条语句（被发送文件的路径）: /Users/carson/Downloads/java.bili/QQClient/src/IMG_4427.JPG");
+                                    System.out.println("Enter this statement (the path to the file being sent,it is subject to be changed)）: /Users/carson/Downloads/java.bili/QQClient/src/IMG_4427.JPG");
                                     String scr = Utility.readString(100);
-                                    System.out.println("输入这条语句把文件发送到这里: /Users/carson/Downloads/java.bili/QQClient/src/qqclient/view/发送过来的第二个哈哈.MOV");
+                                    System.out.println("Enter this statement to send the file here : /Users/carson/Downloads/java.bili/QQClient/src/qqclient/view/发送过来的第二个哈哈.MOV");
                                     String dest = Utility.readString(100);
                                     fileClientService.fileTransfer(scr,dest,userId,getter);
                                     break;
@@ -101,7 +95,7 @@ public class QQView {
                         }
                     }   // end if
                     else{
-                        System.out.println("登陆服务器失败");
+                        System.out.println("Failed to login to the server");
                     }
 
                     break;
