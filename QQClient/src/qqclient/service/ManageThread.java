0b1ae301f1776a3ae8535d83a1159aed70635dd8
Carson-NNY <109368070+Carson-NNY@users.noreply.github.com>
2022-09-11 18:46:02 -0400

    Update ManageThread.java

--- QQClient/src/qqclient/service/ManageThread.java
+++ QQClient/src/qqclient/service/ManageThread.java
@@ -2,21 +2,18 @@ package qqclient.service;
 
 import java.util.HashMap;
 
-/**
- * @author Carson
- * @Version
- */
+
 public class ManageThread {
 
-    // 把多线程放入HashMap集合（好管理）, key是userId，value就是线程
+    // Put multithreading into the HashMap collection (easy to manage).  key is userId,value is thread.
     private static HashMap<String, ClientServiceThread> hm = new HashMap<>();
 
-    // 将某个线程加入到集合
+    // Add a thread to the collection
     public  static void addThread(String userId, ClientServiceThread thread){
         hm.put(userId,thread);
     }
 
-    // 通过userId得到线程
+    // Get threads through userId
     public static ClientServiceThread getClientServiceThread(String userId){
         return hm.get(userId);
     }
